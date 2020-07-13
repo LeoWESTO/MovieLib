7f13171e8f27db26aa560ed4f47e97f0e308c37f
Amir Zagirov <zagirov_amir@mail.ru>
2020-07-13 22:33:20 +0300

    add migrations

---
+++ MovieLib/Migrations/20200713193020_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieLib.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    ID = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    ReleaseYear = table.Column<string>(nullable: true),
+                    PictureURL = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
